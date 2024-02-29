66b600631d3831558df911542a80468ead79bd9f
Oksana Donchuk <80112131+OksanaDonchuk@users.noreply.github.com>
2024-02-29 20:47:36 +0200

    Part 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,16 @@
 import user from '../data/user.json';
-import data from '../data/data.json';
-import friends from '../data/friends.json';
-import transactions from '../data/transaction.json';
+// import data from '../data/data.json';
+// import friends from '../data/friends.json';
+// import transactions from '../data/transaction.json';
+import { Profile } from './Profile/Profile';
 
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      React homework template
+    <div>
+      <div className="title">
+        <h1>1 - Профіль соціальної мережі</h1>
+      </div>
+      <Profile {...user} />
     </div>
   );
 };

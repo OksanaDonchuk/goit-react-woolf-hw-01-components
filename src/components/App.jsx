bab75fa6b6216c56baba62cc20862821eda56292
Oksana Donchuk <80112131+OksanaDonchuk@users.noreply.github.com>
2024-02-29 22:50:46 +0200

    Part 4

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,11 @@
 import user from '../data/user.json';
 import data from '../data/data.json';
 import friends from '../data/friends.json';
-// import transactions from '../data/transaction.json';
+import transactions from '../data/transaction.json';
 import { Profile } from './Profile/Profile';
 import { Statistics } from './Statistics/Statistics';
 import { FriendList } from './FriendList/FriendList';
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
 
 export const App = () => {
   return (
@@ -23,6 +24,11 @@ export const App = () => {
         <h2>3 - Список друзів</h2>
       </div>
       <FriendList friends={friends} />
+
+      <div className="title">
+        <h2>4 - Історія транзакцій</h2>
+      </div>
+      <TransactionHistory items={transactions} />
     </div>
   );
 };
